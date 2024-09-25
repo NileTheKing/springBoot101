1a44f5e3659546a32199007c9ceb77fb80fd0be4
yangnail <wardingplz@naver.com>
2024-09-25 20:45:20 +0900

    Initial commit

---
+++ src/main/java/hello/hello_spring/controller/HomeController.java
@@ -0,0 +1,33 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+
+public class HomeController {
+
+    private final MemberService memberService;
+
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
+}
